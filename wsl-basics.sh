a8043d2f90e1e9575014ca57bcf4b2c54d8b5a70
Maze <matthias.binner41@googlemail.com>
2021-06-16 08:57:06 +0200

    Update wsl-basics.sh

--- wsl-basics.sh
+++ wsl-basics.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo -e "Updating the repositories..."
-sudo apt update -y
-echo -e "Upgrading the currently installed packages..."
-sudo apt upgrade -y
-echo -e "Installing the basic tools for WSL..."
-sudo apt install -y htop vim wget curl git openssh openssl
+work="\e[44;97m[WORK]\e[39;49;1m"
+done="\e[1A\e[42;30m[DONE]\e[39;49;1m"
+echo -e "$work Updating the repositories..."
+sudo apt update -y >> /dev/null 2>&1 
+echo -e "$done Updating the repositories..."
+echo -e "$work Upgrading the currently installed packages..."
+sudo apt upgrade -y >> /dev/null 2>&1 
+echo -e "$done Upgrading the currently installed packages..."
+echo -e "$work Installing the basic tools for WSL..."
+sudo apt install -y htop vim wget curl git openssh-client openssl >> /dev/null 2>&1 
+echo -e "$done Installing the basic tools for WSL..."
